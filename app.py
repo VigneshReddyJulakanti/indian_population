3a2c399e61dd0b1b877883e02be6b204284687d0
VigneshReddyJulakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2021-07-30 16:18:46 +0530

    14

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request
+
+import pickle
+
+app=Flask(__name__)
+
+model=pickle.load(open("model/pop.pkl",'rb'))
+
+def convert_to_string_with_comma(ans):
+        ans=int(ans[0])
+        ans=str(ans)
+        ans=ans[::-1]
+        fans=""
+        for i in range(0,len(ans),3):
+            fans+=f"{ans[i:i+3]},"
+        fans=fans[::-1]
+        fans=fans[1:]
+        return fans
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/submitted',methods=['POST'])
+def submitted():
+    if request.method=="POST":
+        year=request.form["year"]
+        ans=model.predict([[year]])
+
+        ans=convert_to_string_with_comma(ans)
+        
+
+    
+        return render_template("home.html",ans=ans,year=year)
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
